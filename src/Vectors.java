a14581ef4ee73ad11260c730a87eb008fae1e58c
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-03-20 15:53:06 +0100

    Add fields and constructor to Vectors class

--- src/Vectors.java
+++ src/Vectors.java
@@ -2,10 +2,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Vectors {
-    public ArrayList<Double> fetchVector(Scanner scanner) {
+    private ArrayList<Double> vector1;
+    private ArrayList<Double> vector2;
+    private Scanner scanner;
+
+    public Vectors(ArrayList<Double> vector1, ArrayList<Double> vector2, Scanner scanner) {
+        this.vector1 = vector1;
+        this.vector2 = vector2;
+        this.scanner = scanner;
+    }
+
+    public ArrayList<Double> fetchVector() {
         ArrayList<Double> vector = new ArrayList<>();
 
-        String input = scanner.nextLine();
+        String input = this.scanner.nextLine();
         String[] numbers = input.split(" ");
 
         for (String number : numbers) {
@@ -14,5 +24,6 @@ public class Vectors {
 
         return vector;
     }
-    
+
+
 }
