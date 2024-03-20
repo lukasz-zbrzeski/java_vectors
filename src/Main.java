9419dc3621f88c1e7b036fc2a0e7bc789902a9be
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-03-20 17:23:35 +0100

    Create readVector function and add implements action of app

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private static ArrayList<Double> readVector(Vectors vectors, Scanner scanner) {
+        ArrayList<Double> vector = new ArrayList<>();
+
+        boolean cond = true;
+        while (cond) {
+            try {
+                vector = vectors.fetchVector(scanner);
+            } catch (NumberFormatException e) {
+                System.out.print("The given string is not a number. Enter vector again: ");
+                continue;
+            }
+            cond = false;
+        }
+
+        return vector;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Vectors vectors = new Vectors();
+
+        boolean areCorrectVectors = false;
+
+        while (!areCorrectVectors) {
+            System.out.print("Enter first vector: ");
+            ArrayList<Double> vector1 = readVector(vectors, scanner);
+
+            System.out.print("Enter second vector: ");
+            ArrayList<Double> vector2 = readVector(vectors, scanner);
+
+            try {
+                ArrayList<Double> resultOfAddingVectors = vectors.addVectors(vector1, vector2);
+                vectors.saveVector(resultOfAddingVectors);
+            } catch (DifferentLengthVectorsException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            areCorrectVectors = true;
+        }
 
     }
 }
