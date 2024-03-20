fedb55d2578cbddd04a7b4acdcb17e0a69b46570
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-03-20 16:52:51 +0100

    Remove fields and change parameters in functions

--- src/Vectors.java
+++ src/Vectors.java
@@ -2,20 +2,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Vectors {
-    private final ArrayList<Double> vector1;
-    private final ArrayList<Double> vector2;
-    private final Scanner scanner;
+    public Vectors() {}
 
-    public Vectors(ArrayList<Double> vector1, ArrayList<Double> vector2, Scanner scanner) {
-        this.vector1 = vector1;
-        this.vector2 = vector2;
-        this.scanner = scanner;
-    }
-
-    public ArrayList<Double> fetchVector() {
+    public ArrayList<Double> fetchVector(Scanner scanner) {
         ArrayList<Double> vector = new ArrayList<>();
 
-        String input = this.scanner.nextLine();
+        String input = scanner.nextLine();
         String[] numbers = input.split(" ");
 
         for (String number : numbers) {
@@ -25,14 +17,14 @@ public class Vectors {
         return vector;
     }
 
-    public ArrayList<Double> addVectors() throws DifferentLengthVectorsException {
-        if (this.vector1.size() != this.vector2.size()) {
-            throw new DifferentLengthVectorsException(this.vector1.size(), this.vector2.size());
+    public ArrayList<Double> addVectors(ArrayList<Double> vector1, ArrayList<Double> vector2) throws DifferentLengthVectorsException {
+        if (vector1.size() != vector2.size()) {
+            throw new DifferentLengthVectorsException(vector1.size(), vector2.size());
         }
 
         ArrayList<Double> addedVectors = new ArrayList<>();
-        for (int i = 0; i < this.vector1.size(); i++) {
-            Double sum = this.vector1.get(i) + this.vector2.get(i);
+        for (int i = 0; i < vector1.size(); i++) {
+            Double sum = vector1.get(i) + vector2.get(i);
             addedVectors.add(sum);
         }
 
